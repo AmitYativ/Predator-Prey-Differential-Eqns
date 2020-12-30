5fad4a939269fc9a8e1afd8df8eef491ecc065b4
AmitYativ <32584372+AmitYativ@users.noreply.github.com>
2020-12-30 01:43:12 -0800

    Update lotka_volterra_approximator.py

--- lotka_volterra_approximator.py
+++ lotka_volterra_approximator.py
@@ -10,7 +10,14 @@ import matplotlib.pyplot as p
 
 """ Program Description:
 
-This program is written in Python and utilizes methods of approximating ordinary differential equations such as RK4, Forward Euler, and Adams Predictor Corrector Method to approximate periodic solutions of the Lotka-Volterra Equations. This depicts the change of population through time within a predator-prey relationship, such as rabbits (prey) and foxes (predators), portraying the dynamics of biological systems in which the predator and prey species interact.
+This program is written in Python and utilizes methods
+of approximating ordinary differential equations such
+as RK4, Forward Euler, and Adams Predictor Corrector
+Method to approximate periodic solutions of the Lotka-Volterra
+Equations. This depicts the change of population through time
+within a predator-prey relationship, such as rabbits (prey)
+and foxes (predators), portraying the dynamics of biological
+systems in which the predator and prey species interact.
 
 """
 
